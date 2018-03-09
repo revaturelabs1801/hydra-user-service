805e08f23d83a6a72349cbed2e695d580df0bc56
Tanner McDonald <Tanner.McDonald@outlook.com>
2018-03-09 11:05:37 -0500

    Preparing to pull Service information

---
+++ User-Service/src/main/java/com/revature/demo/exception/CustomException.java
@@ -0,0 +1,25 @@
+package com.revature.demo.exception;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.revature.demo.logging.LoggerClass;
+
+public class CustomException extends Exception {
+
+  private static final long serialVersionUID = 8632434538067813982L;
+  private static final Logger logger = LogManager.getLogger(LoggerClass.class);
+
+  public CustomException(Throwable message) {
+    super(message);
+  }
+
+//  public CustomException(MessagingException e) {
+//    logger.error(e);
+//  }
+
+  public CustomException(String string) {
+    super(string);
+  }
+
+}
