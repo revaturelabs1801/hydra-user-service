0f7b1e25a703b5c4bf55819b6aadf53bed4ad7ca
TJay Cargle <tbcargle@fullsail.edu>
2018-03-20 10:12:30 -0400

    Edited services and repos to use Role instead of ints. Also edited application.properties.

--- User-Service/src/main/java/com/revature/demo/repository/BamUserRepository.java
+++ User-Service/src/main/java/com/revature/demo/repository/BamUserRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.revature.demo.beans.BamUser;
+import com.revature.demo.beans.Role;
 
 @RepositoryRestResource
 public interface BamUserRepository extends JpaRepository<BamUser, Integer>{
 	public BamUser findByUserId(int id);
 	public BamUser findByEmail(String email);
 	public List<BamUser> findByBatch(Integer batch);
-	public List<BamUser> findByRole(int role);
+	public List<BamUser> findByRole(Role role);
 	
 	public List<BamUser> findByFNameAndLName(String f, String l);
 }
